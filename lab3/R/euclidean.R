3c37c563fc389e60cb034d6b29b3247cb6a93484
codercola-mw <meconho@gmail.com>
2019-09-12 14:24:36 +0200

    "eulidean function_update" "eulidean RD file_new" "r_lab3 R document_new" "r_lab3 RD file_new"

--- lab3/R/euclidean.R
+++ lab3/R/euclidean.R
@@ -1,4 +1,13 @@
-##1.1
+#' Find the greates common divisior of two numbers
+#' 
+#' @param n1 A number.
+#' @param n2 A number.
+#' @return The greatest common divisior of \code{n1} and \code{n2}.
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#' @references https://en.wikipedia.org/wiki/Euclidean algorithm.
+
 
 euclidean <- function(n1, n2) {
   if(n1 == 0){
@@ -11,4 +20,3 @@ euclidean <- function(n1, n2) {
 }
 
 # 1.1.2
-print(dsdsad)
---
+++ lab3/R/r_lab3.R
@@ -0,0 +1,11 @@
+#' r_lab3: A package for computating the greatest common divisior of two numbers.
+#'
+#' The r_lab3 package provides two important functions:
+#' euclidean and dijkstra.
+#' 
+#' @section r_lab3 functions: 
+#' The euclidean function is to find the greatest common divisor of two numbers.
+#'
+#' @docType package
+#' @name r_lab3
+NULL
